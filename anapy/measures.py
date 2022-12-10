f05c22897330de4b8d43bc4d29f8c59b50debe04
Benn Arthur <87674915+bennarthurdev@users.noreply.github.com>
2022-12-10 18:22:21 -0300

    Update measures.py

--- anapy/measures.py
+++ anapy/measures.py
@@ -15,3 +15,11 @@ def get_central_tendency_measures(series):
 receive a series object, who can be, a column in an dataframe
 returns the variability mostly comum measures for this object
 '''
+def get_variability_measures(series):
+    return pd.Series({
+        "variance" : series.var(),
+        "standard deviation" : series.std(),
+        "shifiting_range" : np.unique(shifting.shifting_range(np.array(series))),
+        "CV": variation(series) 
+    })
+
